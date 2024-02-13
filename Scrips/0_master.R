63834c46eebc8be66213df3554494318b7d535a0
Andres Rengifo <afrj1996@gmail.com>
2024-02-13 18:04:15 -0500

    Create master scrip.

---
+++ Scrips/0_master.R
@@ -0,0 +1,16 @@
+### This is the master Scrip. 
+
+
+## Clear memory
+rm(list = ls())
+
+
+
+# Point 1
+source("Scrips/1_Point1_descrip.R")
+source("Scrips/2_Point1_Regression1.R")
+
+#Point2
+
+source("Scrips/1_Point2_Regression1.R")
+
