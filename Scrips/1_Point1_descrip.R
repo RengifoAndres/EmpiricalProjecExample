cd3e3cee388bebea06594d9e2da48f08b54e971d
Andres Rengifo <afrj1996@gmail.com>
2024-02-13 18:08:06 -0500

    Create 1_Point1_descrip.R

---
+++ Scrips/1_Point1_descrip.R
@@ -0,0 +1,24 @@
+## Point 1 Descriptives 
+
+
+# Load Packages
+
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+
+
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       gridExtra, ## visualizing missing data
+       corrplot, ## Correlation Plots 
+       stargazer, ## tables/output to TEX. 
+       MASS)   
+
+
+## Import data
+
+
+db <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
+db <- as_tibble(db) %>% rename(gender=sex) 
+
+
